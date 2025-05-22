661495578d667ac13e3d21eea1fd68c5276290ae
Lahiru Yasas <lahiruyasas7@gmail.com>
2025-05-22 18:34:55 +0530

    added MyTasks component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import CalenderComponent from "./components/Home/CalenderComponent";
+import MyTasksComponent from "./components/Home/MyTasksComponent";
 
 export default function Home() {
   return (
-    <div>
+    <div className="flex gap-5">
       <CalenderComponent/>
+      <MyTasksComponent/>
     </div>
     // <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
     // </div>
