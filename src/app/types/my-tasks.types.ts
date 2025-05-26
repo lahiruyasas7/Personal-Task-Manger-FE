2bd74a0516ce1d80e8c8a1a3a761e0760bc4e30a
Lahiru Yasas <lahiruyasas7@gmail.com>
2025-05-26 23:42:29 +0530

    created MYTask type

---
+++ src/app/types/my-tasks.types.ts
@@ -0,0 +1,10 @@
+export interface MyTasks {
+  id: string;
+  title: string;
+  description: string;
+  dueDate: string;
+  status: string;
+  user_id: string;
+  created_at: string;
+  updated_at: string;
+}
