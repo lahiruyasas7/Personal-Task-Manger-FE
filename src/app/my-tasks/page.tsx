ab7c81437e611a519ffba195f91952c22ae8ed4e
Lahiru Yasas <lahiruyasas7@gmail.com>
2025-05-28 17:29:38 +0530

    added ThiWeekTasks component

--- src/app/my-tasks/page.tsx
+++ src/app/my-tasks/page.tsx
@@ -1,13 +1,15 @@
-'use client'
+"use client";
 import React from "react";
 import TodayTasks from "../components/MyTasks/TodayTasks";
 import TomorrowTasks from "../components/MyTasks/TomorrowTasks";
+import ThisWeekTasks from "../components/MyTasks/ThisWeekTasks";
 
 function MyTasks() {
   return (
     <div className="flex flex-col">
       <TodayTasks />
-      <TomorrowTasks/>
+      <TomorrowTasks />
+      <ThisWeekTasks />
     </div>
   );
 }
